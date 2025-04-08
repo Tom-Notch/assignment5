b94721a0a269f74e77ca9fe7ab0982a6a95e350f
Tom Notch <topnotchymk@gmail.com>
2025-04-08 17:21:00 -0400

    Formatting & fixing typo

--- eval_cls.py
+++ eval_cls.py
@@ -1,59 +1,72 @@
-import numpy as np
+#!/usr/bin/env python3
 import argparse
 
+import numpy as np
 import torch
+
 from models import cls_model
 from utils import create_dir
 
+
 def create_parser():
-    """Creates a parser for command-line arguments.
-    """
+    """Creates a parser for command-line arguments."""
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--num_cls_class', type=int, default=3, help='The number of classes')
-    parser.add_argument('--num_points', type=int, default=10000, help='The number of points per object to be included in the input data')
+    parser.add_argument(
+        "--num_cls_class", type=int, default=3, help="The number of classes"
+    )
+    parser.add_argument(
+        "--num_points",
+        type=int,
+        default=10000,
+        help="The number of points per object to be included in the input data",
+    )
 
     # Directories and checkpoint/sample iterations
-    parser.add_argument('--load_checkpoint', type=str, default='model_epoch_0')
-    parser.add_argument('--i', type=int, default=0, help="index of the object to visualize")
+    parser.add_argument("--load_checkpoint", type=str, default="model_epoch_0")
+    parser.add_argument(
+        "--i", type=int, default=0, help="index of the object to visualize"
+    )
 
-    parser.add_argument('--test_data', type=str, default='./data/cls/data_test.npy')
-    parser.add_argument('--test_label', type=str, default='./data/cls/label_test.npy')
-    parser.add_argument('--output_dir', type=str, default='./output')
+    parser.add_argument("--test_data", type=str, default="./data/cls/data_test.npy")
+    parser.add_argument("--test_label", type=str, default="./data/cls/label_test.npy")
+    parser.add_argument("--output_dir", type=str, default="./output")
 
-    parser.add_argument('--exp_name', type=str, default="exp", help='The name of the experiment')
+    parser.add_argument(
+        "--exp_name", type=str, default="exp", help="The name of the experiment"
+    )
 
     return parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
-    args.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     create_dir(args.output_dir)
 
     # ------ TO DO: Initialize Model for Classification Task ------
-    model = 
-    
+    model = None
+
     # Load Model Checkpoint
-    model_path = './checkpoints/cls/{}.pt'.format(args.load_checkpoint)
-    with open(model_path, 'rb') as f:
+    model_path = "./checkpoints/cls/{}.pt".format(args.load_checkpoint)
+    with open(model_path, "rb") as f:
         state_dict = torch.load(f, map_location=args.device)
         model.load_state_dict(state_dict)
     model.eval()
-    print ("successfully loaded checkpoint from {}".format(model_path))
-
+    print("successfully loaded checkpoint from {}".format(model_path))
 
     # Sample Points per Object
-    ind = np.random.choice(10000,args.num_points, replace=False)
-    test_data = torch.from_numpy((np.load(args.test_data))[:,ind,:])
+    ind = np.random.choice(10000, args.num_points, replace=False)
+    test_data = torch.from_numpy((np.load(args.test_data))[:, ind, :])
     test_label = torch.from_numpy(np.load(args.test_label))
 
     # ------ TO DO: Make Prediction ------
-    pred_label = 
+    pred_label = None
 
     # Compute Accuracy
-    test_accuracy = pred_label.eq(test_label.data).cpu().sum().item() / (test_label.size()[0])
-    print ("test accuracy: {}".format(test_accuracy))
-
+    test_accuracy = pred_label.eq(test_label.data).cpu().sum().item() / (
+        test_label.size()[0]
+    )
+    print("test accuracy: {}".format(test_accuracy))
--- utils.py
+++ utils.py
@@ -1,20 +1,22 @@
+#!/usr/bin/env python3
 import os
-import torch
-import pytorch3d
-from pytorch3d.renderer import (
-    AlphaCompositor,
-    PointsRasterizationSettings,
-    PointsRenderer,
-    PointsRasterizer,
-)
+
 import imageio
+import pytorch3d
+import torch
+from pytorch3d.renderer import AlphaCompositor
+from pytorch3d.renderer import PointsRasterizationSettings
+from pytorch3d.renderer import PointsRasterizer
+from pytorch3d.renderer import PointsRenderer
+
 
 def save_checkpoint(epoch, model, args, best=False):
     if best:
-        path = os.path.join(args.checkpoint_dir, 'best_model.pt')
+        path = os.path.join(args.checkpoint_dir, "best_model.pt")
     else:
-        path = os.path.join(args.checkpoint_dir, 'model_epoch_{}.pt'.format(epoch))
+        path = os.path.join(args.checkpoint_dir, "model_epoch_{}.pt".format(epoch))
     torch.save(model.state_dict(), path)
+
 
 def create_dir(directory):
     """
@@ -22,6 +24,7 @@ def create_dir(directory):
     """
     if not os.path.exists(directory):
         os.makedirs(directory)
+
 
 def get_points_renderer(
     image_size=256, device=None, radius=0.01, background_color=(1, 1, 1)
@@ -35,7 +38,7 @@ def get_points_renderer(
             will automatically use GPU if available, otherwise CPU.
         radius (float): The radius of the rendered point in NDC.
         background_color (tuple): The background color of the rendered image.
-    
+
     Returns:
         PointsRenderer.
     """
@@ -44,7 +47,10 @@ def get_points_renderer(
             device = torch.device("cuda:0")
         else:
             device = torch.device("cpu")
-    raster_settings = PointsRasterizationSettings(image_size=image_size, radius=radius,)
+    raster_settings = PointsRasterizationSettings(
+        image_size=image_size,
+        radius=radius,
+    )
     renderer = PointsRenderer(
         rasterizer=PointsRasterizer(raster_settings=raster_settings),
         compositor=AlphaCompositor(background_color=background_color),
@@ -52,37 +58,49 @@ def get_points_renderer(
     return renderer
 
 
-def viz_seg (verts, labels, path, device):
+def viz_seg(verts, labels, path, device):
     """
     visualize segmentation result
     output: a 360-degree gif
     """
-    image_size=256
-    background_color=(1, 1, 1)
-    colors = [[1.0,1.0,1.0], [1.0,0.0,1.0], [0.0,1.0,1.0],[1.0,1.0,0.0],[0.0,0.0,1.0], [1.0,0.0,0.0]]
+    image_size = 256
+    background_color = (1, 1, 1)
+    colors = [
+        [1.0, 1.0, 1.0],
+        [1.0, 0.0, 1.0],
+        [0.0, 1.0, 1.0],
+        [1.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [1.0, 0.0, 0.0],
+    ]
 
     # Construct various camera viewpoints
     dist = 3
     elev = 0
-    azim = [180 - 12*i for i in range(30)]
-    R, T = pytorch3d.renderer.cameras.look_at_view_transform(dist=dist, elev=elev, azim=azim, device=device)
+    azim = [180 - 12 * i for i in range(30)]
+    R, T = pytorch3d.renderer.cameras.look_at_view_transform(
+        dist=dist, elev=elev, azim=azim, device=device
+    )
     c = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, fov=60, device=device)
 
-    sample_verts = verts.unsqueeze(0).repeat(30,1,1).to(torch.float)
+    sample_verts = verts.unsqueeze(0).repeat(30, 1, 1).to(torch.float)
     sample_labels = labels.unsqueeze(0)
-    sample_colors = torch.zeros((1,10000,3))
+    sample_colors = torch.zeros((1, 10000, 3))
 
     # Colorize points based on segmentation labels
     for i in range(6):
-        sample_colors[sample_labels==i] = torch.tensor(colors[i])
+        sample_colors[sample_labels == i] = torch.tensor(colors[i])
 
-    sample_colors = sample_colors.repeat(30,1,1).to(torch.float)
+    sample_colors = sample_colors.repeat(30, 1, 1).to(torch.float)
 
-    point_cloud = pytorch3d.structures.Pointclouds(points=sample_verts, features=sample_colors).to(device)
+    point_cloud = pytorch3d.structures.Pointclouds(
+        points=sample_verts, features=sample_colors
+    ).to(device)
 
-    renderer = get_points_renderer(image_size=image_size, background_color=background_color, device=device)
-    rend = renderer(point_cloud, cameras=c).cpu().numpy() # (30, 256, 256, 3)
+    renderer = get_points_renderer(
+        image_size=image_size, background_color=background_color, device=device
+    )
+    rend = renderer(point_cloud, cameras=c).cpu().numpy()  # (30, 256, 256, 3)
     rend = (rend * 255).astype(np.uint8)
 
     imageio.mimsave(path, rend, fps=15)
-
